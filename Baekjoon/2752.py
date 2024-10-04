a411d2932fd1922acccf3474cdb7ecdd7bb5ef57
rhan0134 <yurhan.0134@gmail.com>
2024-10-05 02:10:32 +0900

    TIL Day33

--- Baekjoon/2752.py
+++ Baekjoon/2752.py
@@ -17,18 +17,40 @@ print(*li)
 A = list(map(int, input().split()))
 
 def insertion_sort(li, n):
-    if n == len(li): # 탈출 조건
+    if n <= 1: # 탈출 조건(배열의 크기가 1이면 정렬된 것으로 간주)
         return li  # 정렬된 리스트를 반환
-    else:
-        key = li[n] # 이번에 삽입할 요소
-        i = 0
-        while i < n and li[i] < key: # key가 들어갈 위치를 찾기(for문 보다 while문이 효율적)
-          i += 1
-        li.insert(i, key)
-        del li[n + 1]  # 마지막 요소 삭제 (중복 방지)
-        
-        return insertion_sort(li, n + 1)  # 재귀 호출
 
-n = 1
-sorted_list = insertion_sort(A, n)
+    # 재귀 호출하여 앞의 n-1개를 정렬
+    insertion_sort(li, n - 1)
+    key = li[n-1] # 이번에 정렬해야 하는 요소
+    i = n-2
+
+    #key보다 큰 요소들을 오른쪽으로 이동
+    while i >= 0 and li[i] > key:
+      li[i+1] = li[i]
+      i -= 1
+
+    # key 삽입  
+    li[i+1] = key
+    return li
+
+sorted_list = insertion_sort(A, len(A))
+print(*sorted_list)
+
+# 삽입정렬 > 상향식(왼쪽 항목부터 차례로 비교)
+A = list(map(int, input().split()))
+
+def insertion_sort(li):
+  for i in range(1, len(li)):
+    key = li[i] #이번에 삽입할 요소
+    j = 0
+  
+    while j < i and li[j] < key: # key가 들어갈 위치를 찾기(for문 보다 while문이 효율적)
+      j += 1
+    li.insert(j, key)
+    del li[i + 1]  # 요소 삭제 (중복 방지)
+        
+  return li  # 정렬된 리스트 반환
+
+sorted_list = insertion_sort(A)
 print(*sorted_list)
