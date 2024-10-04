9819900c15c58ba2ab1b7a7eaa3d52bc57e4b628
rhan0134 <yurhan.0134@gmail.com>
2024-10-05 01:16:33 +0900

    TIL Day33

--- Baekjoon/2752.py
+++ Baekjoon/2752.py
@@ -12,3 +12,23 @@ for i in range(len(li)-1):
       li[i], li[j] = li[j], li[i]
 
 print(*li)
+
+# 삽입 정렬 > 하향식(순환 구조)
+A = list(map(int, input().split()))
+
+def insertion_sort(li, n):
+    if n == len(li): # 탈출 조건
+        return li  # 정렬된 리스트를 반환
+    else:
+        key = li[n] # 이번에 삽입할 요소
+        i = 0
+        while i < n and li[i] < key: # key가 들어갈 위치를 찾기(for문 보다 while문이 효율적)
+          i += 1
+        li.insert(i, key)
+        del li[n + 1]  # 마지막 요소 삭제 (중복 방지)
+        
+        return insertion_sort(li, n + 1)  # 재귀 호출
+
+n = 1
+sorted_list = insertion_sort(A, n)
+print(*sorted_list)
